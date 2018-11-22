a32b690c7bab89dbb3205cbb9a3fae3c1326cb14
cjkcr <cjkcrbb@gmail.com>
2018-11-22 22:03:53 +0800

    练习

--- abcd.py
+++ abcd.py
@@ -39,7 +39,7 @@
 
 def break_words(stuff):
     '''this function will break up words for us.'''
-    words = stuff.split('')
+    words = stuff.split(' ')
     return words
 def sort_words(words):
     '''sorts the words.'''
--- goohtml.py
+++ goohtml.py
@@ -1,11 +1,11 @@
-from bs4 import BeautifulSoup
-import requests
-
-date=requests.get('http://www.google.com').content
-
-soup=BeautifulSoup(date,'html.parser')
-
-print(soup.title)
+# from bs4 import BeautifulSoup
+# import requests
+#
+# date=requests.get('http://www.google.com').content
+#
+# soup=BeautifulSoup(date,'html.parser')
+#
+# print(soup.title)
 
 # a,b=0,1
 # while b<10:
@@ -19,3 +19,29 @@ print(soup.title)
 #         return n*num(n-1)
 #
 # print(num(10))
+import math
+import random
+
+import abcd
+sentence = ('all good things come to those who wait.')
+words = abcd.break_words(sentence)
+print(words)
+sorted_words = abcd.sort_words(words)
+print(sorted_words)
+
+num = math.exp(1)
+print(num)
+
+print(abs(-100))
+print(math.ceil(8.12))
+print(math.fabs(-6.222))
+print(math.floor(9.99999))
+print(math.log(10,100))
+print(random.choice(range(10,20)))
+print(random.choice((0,1,2,3)))
+
+if range(4)==[0,1,2,3]:
+    print('yess')
+else:
+    print('noo')
+
